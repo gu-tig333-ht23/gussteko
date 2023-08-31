5841b782714a23badc5ecbd9ea3b94028a6604e2
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-08-31 15:25:38 +0200

    UI och knappar
    Lite förberedande knappar och lite UI justeringar. Knapparna har inte funktioner än.

--- lib/main.dart
+++ lib/main.dart
@@ -31,26 +31,32 @@ class ListPage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text('Todo List'),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-            ),
+          actions: <Widget>[
+            PopupMenuButton(itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(child: Text('Alphabetical')),
+                PopupMenuItem(child: Text('Done')),
+                PopupMenuItem(child: Text('Not Done')),
+              ];
+            })
           ],
         ),
         body: ListView(
-          children: const <Widget>[
+          children: <Widget>[
             ListTile(
               leading: Icon(Icons.check_box_outline_blank),
               title: Text('Klippa gräset'),
+              onTap: () {},
             ),
             ListTile(
               leading: Icon(Icons.check_box_outline_blank),
               title: Text('Köpa snus'),
+              onTap: () {},
             ),
             ListTile(
               leading: Icon(Icons.check_box_outline_blank),
               title: Text('Deklarera'),
+              onTap: () {},
             ),
           ],
         ),
@@ -61,7 +67,7 @@ class ListPage extends StatelessWidget {
               MaterialPageRoute(builder: (context) => const AddPage()),
             );
           },
-          child: const Text('Add Item'),
+          child: const Icon(Icons.add),
         ));
   }
 }
@@ -76,12 +82,7 @@ class AddPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Add Item'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.menu),
-          ),
-        ],
+        actions: [],
       ),
       body: Center(
           child: TextField(
@@ -92,8 +93,9 @@ class AddPage extends StatelessWidget {
         onPressed: () {
           Navigator.pop(context);
         },
-        child: const Text('Add Item'),
+        child: const Text('Add item to your list'),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
