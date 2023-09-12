46d02e4f8ff300eef45a50aaf549bb5cb71d985b
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-09-12 12:15:23 +0200

    Update test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -1,1 +1,125 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const ListPage(title: 'Todo Listpage'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ListPage extends StatelessWidget {
+  const ListPage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Todo List'),
+          actions: <Widget>[
+            PopupMenuButton(itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(child: Text('Alphabetical')),
+                PopupMenuItem(child: Text('Done')),
+                PopupMenuItem(child: Text('Not Done')),
+              ];
+            })
+          ],
+        ),
+        body: Column(children: [
+          _item('Kostashios', 'Master Coder'),
+          _item('Sassa', 'Junior Coder'),
+          _item('Norén', 'Master UX'),
+        ]));
+  }
+
+  Widget _item(String name, String role) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: Colors.blue),
+            ),
+          ),
+          Expanded(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                Text(role),
+              ])),
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(Icons.chevron_right, size: 40),
+          ),
+        ]);
+  }
+}
+
+class AddPage extends StatelessWidget {
+  const AddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Add Item'),
+        actions: [],
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Add item',
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
