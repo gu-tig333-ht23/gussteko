3b7cd404173a74ddf9afde5508a41a7b39217349
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-09-15 10:38:41 +0200

    Kom över tröskeln och fick till providers efter mycket tester

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,17 @@ import 'package:provider/provider.dart';
 class MyState extends ChangeNotifier {
   String _name = '';
   String _description = '';
+  final List<TodoItem> _items = [
+    TodoItem('This is an example', 'Examples looks like this'),
+    TodoItem('To complete an item', 'Tap the checkbox to the left'),
+    TodoItem('To remove an item', 'Tap the bin to the right'),
+    TodoItem('To add a new item', 'Tap the button on your lower right'),
+  ];
   bool _done = false;
 
   String get name => _name;
   String get description => _description;
+  List<TodoItem> get items => _items;
   bool get done => _done;
 
   void setName(String name) {
@@ -20,14 +27,29 @@ class MyState extends ChangeNotifier {
     notifyListeners();
   }
 
+  void addItem(TodoItem item) {
+    _items.add(item);
+    notifyListeners();
+  }
+
   void setDone(bool done) {
     _done = done;
+    notifyListeners();
+  }
+
+  void deleteItem(int index) {
+    _items.removeAt(index);
     notifyListeners();
   }
 }
 
 void main() {
-  runApp(TodoApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MyState(),
+      child: TodoApp(),
+    ),
+  );
 }
 
 class TodoApp extends StatelessWidget {
@@ -56,21 +78,13 @@ class TodoItem {
 }
 
 //Förstasidan där listan visas, builder för listan
-// och en knapp för att byta sida
 class ListPage extends StatelessWidget {
   ListPage({super.key, required this.title});
   final String title;
 
   @override
   Widget build(BuildContext context) {
-    List<TodoItem> items = [
-      TodoItem('Klippa gräset', 'Trädgården och bakom skjulet'),
-      TodoItem('Köpa snus', 'Två stockar lös till pappa'),
-      TodoItem('Städa', 'Disk och tvätt innan sambon slutar'),
-      TodoItem('Parkera om bilen', 'Städdag på gatan tordag 20/9'),
-      TodoItem('Plugga', 'Gör modul 2 i flutterkursen'),
-      TodoItem('Skriv arg lapp', 'Grannen spelar hög musik varje kväll'),
-    ];
+    List<TodoItem> items = context.watch<MyState>().items;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -80,7 +94,6 @@ class ListPage extends StatelessWidget {
           PopupMenuButton(
             itemBuilder: (BuildContext context) {
               return [
-                PopupMenuItem(child: Text('Alphabetical')),
                 PopupMenuItem(child: Text('Done')),
                 PopupMenuItem(child: Text('Not Done')),
               ];
@@ -90,7 +103,7 @@ class ListPage extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
-          return TodoListCreator(items[index]);
+          return TodoListCreator(items[index], items);
         },
         itemCount: items.length,
       ),
@@ -114,49 +127,56 @@ class ListPage extends StatelessWidget {
 // Skapar listan och listelement av todo:er
 class TodoListCreator extends StatelessWidget {
   final TodoItem item;
-  TodoListCreator(this.item, {super.key});
+  final List<TodoItem> items;
+
+  TodoListCreator(this.item, this.items, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {},
       child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: IconButton(
-                icon: Icon(Icons.check_box_outline_blank),
-                onPressed: () {
-                  print('Checked');
-                },
-              ),
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: IconButton(
+              icon: Icon(Icons.check_box_outline_blank),
+              onPressed: () {
+                print('Checked');
+              },
             ),
-            Expanded(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                  Text(
-                    item.name,
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  item.name,
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
-                  Text(item.description),
-                ])),
-            Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: IconButton(
-                  //color: Colors.red,
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    print('Delete');
-                  },
-                )),
-          ]),
+                ),
+                Text(item.description),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: IconButton(
+              //color: Colors.red,
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                int currentIndex = items.indexOf(item);
+                context.read<MyState>().deleteItem(currentIndex);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -165,6 +185,8 @@ class TodoListCreator extends StatelessWidget {
 class AddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String itemName = '';
+    String itemDescription = '';
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -173,8 +195,8 @@ class AddPage extends StatelessWidget {
         actions: [],
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
@@ -182,20 +204,38 @@ class AddPage extends StatelessWidget {
               child: TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Add item',
+                  labelText: 'Item Name',
                 ),
                 onChanged: (text) {
-                  context.read<MyState>().setName(text);
+                  itemName = text;
                 },
               ),
             ),
+            SizedBox(height: 20),
+            Container(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Item Description',
+                ),
+                onChanged: (text) {
+                  itemDescription = text;
+                },
+              ),
+            ),
+            SizedBox(height: 20),
             Container(
               height: 50,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.pop(context);
+                  if (itemName != '') {
+                    TodoItem newItem = TodoItem(itemName, itemDescription);
+                    context.read<MyState>().addItem(newItem);
+                    Navigator.pop(context);
+                  }
                 },
-                child: const Icon(Icons.add),
+                child: const Text('Add Item'),
               ),
             )
           ],
